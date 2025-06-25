fb5a035c9d88c8d4f3ee5290af2bcc2b13317893
Giovanni Bartolini <giovannibartolini93@gmail.com>
2025-06-25 11:35:44 +0200

    correzione usando solo variabili semplici e cicli

--- Studenti/GiovanniBartolini/esercizio4.py
+++ Studenti/GiovanniBartolini/esercizio4.py
@@ -5,10 +5,16 @@
 nomi = ["Anna", "Marco", "Elena", "Osvaldo", "Luca", "Irene", "Giorgio", "Umberto"]
 print(f"Nomi originali: {nomi}")
 
+lista_vocali_maiuscole = ['A', 'E', 'I', 'O', 'U']
+
 nomi_vocali = []
 nomi_consonanti = []
 for nome in nomi:
-    if nome[0].lower() in 'aeiou':
+    inizia_con_vocale = False
+    for vocale in lista_vocali_maiuscole:
+        if nome[0] == vocale:
+            inizia_con_vocale = True
+    if inizia_con_vocale:
         nomi_vocali.append(nome)
     else:
         nomi_consonanti.append(nome)
