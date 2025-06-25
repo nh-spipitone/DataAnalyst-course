f532aa30e93203cd663a0ff1e20b249d1b992c6a
M4raPi <maratundo88@gmail.com>
2025-06-25 10:37:47 +0200

    Eseritazione 1

---
+++ Studenti/MaraAntuofermo/ListeeComprensionni.py
@@ -0,0 +1,19 @@
+numbers = [12, 7, 9, 20, 33, 4, 18]
+
+numeri_pari = []
+numeri_quadri = []
+for n in numbers: 
+    if n % 2 == 0:
+        numeri_pari.append(n)
+    numeri_quadri.append(n**2)
+
+print(numeri_pari)
+print(numeri_quadri)
+
+print("La lunghezza dei numeri pari è: ", len(numeri_pari))
+print("La lunghezza dei numeri quadrati è: ", len(numeri_quadri))
+
+
+
+
+
