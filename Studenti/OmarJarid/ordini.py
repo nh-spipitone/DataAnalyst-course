a958d5fb6fd2fff42e441ee4562eb2e734feade7
Omar Jarid <omarjarid1993@gmail.com>
2025-07-01 09:39:26 +0200

    Update ordini.py

--- Studenti/OmarJarid/ordini.py
+++ Studenti/OmarJarid/ordini.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def carica_dati(percorso: str) -> pd.DataFrame:
     df = pd.read_csv(percorso)
@@ -30,3 +31,17 @@ print(fatturato_clienti)
 
 prodotto_piu_venduto = df.groupby("Prodotto")["Quantità"].sum().idxmax()
 print(prodotto_piu_venduto)
+
+fatturato_giornaliero = (df.groupby("Data")["Totale"].sum().reset_index())
+print(fatturato_giornaliero)
+
+plt.figure(figsize = (10, 6))
+plt.bar(fatturato_giornaliero["Data"], fatturato_giornaliero["Totale"], color = "skyblue")
+plt.xlabel("Data", fontsize = 12)
+plt.ylabel("Fatturato (€)", fontsize = 12)
+plt.title("Fatturato giornaliero", fontsize = 16)
+plt.tight_layout()
+plt.show()
+
+media_prezzo_per_prodotto = (df.groupby("Prodotto")["Prezzo_unitario"].mean().reset_index())
+print(media_prezzo_per_prodotto)
