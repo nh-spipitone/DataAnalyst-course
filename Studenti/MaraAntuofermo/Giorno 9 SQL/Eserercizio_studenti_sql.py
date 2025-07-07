65ae232031f56fdae4f2be2194c8e22f6c02dc70
M4raPi <maratundo88@gmail.com>
2025-07-07 10:44:25 +0200

    Aggiungi creazione della tabella 'schiavi' nel database e modifica la connessione nel file di esercizio sugli studenti

--- Studenti/MaraAntuofermo/Giorno 9 SQL/Eserercizio_studenti_sql.py
+++ Studenti/MaraAntuofermo/Giorno 9 SQL/Eserercizio_studenti_sql.py
@@ -1,5 +1,18 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
 
-engine = create_engine("postgresql+psycopg2://myuser:mypassword@localhost:5432/mydatabase")
+# Creazione connessione al database
+engine = create_engine(
+    "postgresql+psycopg2://myuser:mypassword@localhost:5432/mydatabase"
+)
 
+with engine.begin() as conn:
+    conn.execute(
+        text("""CREATE TABLE IF NOT EXISTS schiavi (
+                id           SERIAL PRIMARY KEY,
+                studente VARCHAR(50),
+                materia VARCHAR(50),
+                voto INTEGER);"""))
+
+tabella = pd.read_sql("SELECT * FROM studenti;", con=engine)
+print(tabella)
